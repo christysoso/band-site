c67bdf453dd8e9c63c260499cd073760f00bcc80
christysoso <christyzys@gmail.com>
2021-10-24 11:34:35 -0700

    Prettiered code, added comments to section out code

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,126 +1,6 @@
-// const showTimes = [
-//   {
-//     date: "Mon Sept 06 2021",
-//     venue: "Ronald Lane",
-//     location: "San Franciso, Ca",
-//   },
-//   {
-//     date: "Tue Sept 21 2021",
-//     venue: "Pier 3 East",
-//     location: "San Franciso, Ca",
-//   },
-
-//   {
-//     date: "Fri Oct 15 2021",
-//     venue: "View Lounge",
-//     location: "San Franciso, Ca",
-//   },
-
-//   {
-//     date: "Sat Nov 06 2021",
-//     venue: "Hyatt Agency",
-//     location: "San Franciso, Ca",
-//   },
-
-//   {
-//     date: "Fri Nov 26 2021",
-//     venue: "Moscow Center",
-//     location: "San Franciso, Ca",
-//   },
-
-//   {
-//     date: "Wed Dec 15 2021",
-//     venue: "Press Club",
-//     location: "San Franciso, Ca",
-//   },
-// ];
+//List shows from API
 
 const showList = document.querySelector(".show-info__wrapper");
-
-// function listShows() {
-//   for (let i = 0; i < showTimes.length; i++) {
-//     //create show-info container
-//     const showContainerElem = document.createElement("section");
-//     showContainerElem.classList.add("show-info__container");
-//     showContainerElem.classList.add("show-hover");
-//     showContainerElem.addEventListener("click", function (event) {
-//       showContainerElem.style.backgroundColor = "#e1e1e1";
-//     });
-
-//     //create div container wrapper
-//     const showContainerWrapper = document.createElement("div");
-//     showContainerWrapper.classList.add("show-info__container--wrapper");
-
-//     //create header in container wrapper
-//     const showHeader = document.createElement("h4");
-//     showHeader.classList.add("show-info__container--title");
-//     showHeader.classList.add("hide-title");
-//     showHeader.innerText = "Date";
-
-//     //create date details
-//     const showDetailsDate = document.createElement("p");
-//     showDetailsDate.classList.add("show-info__container--info");
-//     showDetailsDate.classList.add("date");
-//     showDetailsDate.innerText = showTimes[i].date;
-
-//     //create div container wrapper2
-//     const showContainerWrapper2 = document.createElement("div");
-//     showContainerWrapper2.classList.add("show-info__container--wrapper");
-
-//     //create header in container wrapper2
-//     const showHeader2 = document.createElement("h4");
-//     showHeader2.classList.add("show-info__container--title");
-//     showHeader2.classList.add("hide-title");
-//     showHeader2.innerText = "Venue";
-
-//     //create venue details
-//     const showDetailsVenue = document.createElement("p");
-//     showDetailsVenue.classList.add("show-info__container--info");
-//     showDetailsVenue.innerText = showTimes[i].venue;
-
-//     //create div container wrapper3
-//     const showContainerWrapper3 = document.createElement("div");
-//     showContainerWrapper3.classList.add("show-info__container--wrapper");
-
-//     //create header in container wrapper3
-//     const showHeader3 = document.createElement("h4");
-//     showHeader3.classList.add("show-info__container--title");
-//     showHeader3.classList.add("hide-title");
-//     showHeader3.innerText = "Location";
-
-//     //create location details
-//     const showDetailsLocation = document.createElement("p");
-//     showDetailsLocation.classList.add("show-info__container--info");
-//     showDetailsLocation.innerText = showTimes[i].location;
-
-//     //create button
-//     const showBuyBtn = document.createElement("button");
-//     showBuyBtn.classList.add("show-info__btn");
-//     showBuyBtn.innerText = "BUY TICKETS";
-
-//     //append created elements
-
-//     showContainerElem.appendChild(showContainerWrapper);
-//     showContainerWrapper.appendChild(showHeader);
-//     showContainerWrapper.appendChild(showDetailsDate);
-//     showContainerElem.appendChild(showContainerWrapper2);
-//     showContainerWrapper2.appendChild(showHeader2);
-//     showContainerWrapper2.appendChild(showDetailsVenue);
-//     showContainerElem.appendChild(showContainerWrapper3);
-//     showContainerWrapper3.appendChild(showHeader3);
-//     showContainerWrapper3.appendChild(showDetailsLocation);
-//     showContainerElem.appendChild(showBuyBtn);
-
-//     //finally glue everything together
-//     showList.appendChild(showContainerElem);
-//   }
-// }
-
-const usersURL =
-  "https://project-1-api.herokuapp.com/showdates?api_key=f973b4c7-6184-4c3e-9367-d192acecacd3";
-axios.get(usersURL).then((response) => {
-  console.log(response.data);
-});
 
 function listShows() {
   axios
@@ -128,8 +8,6 @@ function listShows() {
       `https://project-1-api.herokuapp.com/showdates?api_key=f973b4c7-6184-4c3e-9367-d192acecacd3`
     )
     .then((response) => {
-      console.log(response.data);
-
       response.data.forEach((shows) => {
         console.log(shows);
 
@@ -137,15 +15,10 @@ function listShows() {
         const showContainerElem = document.createElement("section");
         showContainerElem.classList.add("show-info__container");
         showContainerElem.classList.add("show-hover");
-        
-        
-  
 
         showContainerElem.addEventListener("click", function (event) {
-            showContainerElem.classList.toggle('ClickEvent');
+          showContainerElem.classList.toggle("ClickEvent");
         });
-
-      
 
         //create div container wrapper
         const showContainerWrapper = document.createElement("div");
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,19 +1,12 @@
-const usersURL =
-  "https://project-1-api.herokuapp.com/comments?api_key=f973b4c7-6184-4c3e-9367-d192acecacd3";
-axios.get(usersURL).then((response) => {
-  console.log(response.data);
-});
-
 const commentsElem = document.querySelector(".comments__all-container");
 
+// Append preexisting documents from API with get
 function displayComments() {
   axios
     .get(
       `https://project-1-api.herokuapp.com/comments?api_key=f973b4c7-6184-4c3e-9367-d192acecacd3`
     )
     .then((response) => {
-      console.log(response.data);
-
       const sortedComments = response.data.sort(
         (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
       );
@@ -84,116 +77,9 @@ function displayComments() {
 
 displayComments();
 
-// const newComments = [
-//   {
-//     name: "Connor Walton",
-//     date: "02/17/2021",
-//     comments:
-//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-//     avatar: "./Assets/Images/placeholderavatar.png",
-//   },
-
-//   {
-//     name: "Emilie Beach",
-//     date: "01/01/2021",
-//     comments:
-//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-//     avatar: "./Assets/Images/placeholderavatar.png",
-//   },
-//   {
-//     name: "Miles Acosta",
-//     date: "12/20/2020",
-//     comments:
-//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//     avatar: "./Assets/Images/placeholderavatar.png",
-//   },
-// ];
-
-// const commentsElem = document.querySelector(".comments__all-container");
-
-// function displayComments() {
-//   commentsElem.innerHTML = "";
-//   for (let i = 0; i < newComments.length; i++) {
-//     //create comments container
-//     const commentsContainer = document.createElement("article");
-//     commentsContainer.classList.add("comments__container");
-
-//     //create comments container left
-//     const containerLeft = document.createElement("div");
-//     containerLeft.classList.add("comment__container--left");
-
-//     //create image connect to left container
-//     const containerImg = document.createElement("img");
-//     containerImg.classList.add("comments__container--pic");
-//     containerImg.setAttribute("src", newComments[i].avatar);
-//     containerImg.setAttribute("alt", "profile-pic");
-
-//     //create comments container right
-//     const containerRight = document.createElement("div");
-//     containerRight.classList.add("comments__container--right");
-
-//     //create comments header
-//     const commentsHeader = document.createElement("div");
-//     commentsHeader.classList.add("comments__container--header");
-
-//     //create comments name
-//     const commentsName = document.createElement("h4");
-//     commentsName.classList.add("comments__container--name");
-//     commentsName.innerText = newComments[i].name;
-
-//     //create comments date
-//     const commentsDate = document.createElement("h4");
-//     commentsDate.classList.add("comments__container--date");
-//     commentsDate.innerText = newComments[i].date;
-
-//     //create comments paragraph
-//     const commentsPara = document.createElement("p");
-//     commentsPara.classList.add("comments__container--comment");
-//     commentsPara.innerText = newComments[i].comments;
-
-//     //Append elements together
-//     commentsContainer.appendChild(containerLeft);
-//     containerLeft.appendChild(containerImg);
-//     commentsContainer.appendChild(containerRight);
-//     containerRight.appendChild(commentsHeader);
-//     commentsHeader.appendChild(commentsName);
-//     commentsHeader.appendChild(commentsDate);
-//     containerRight.appendChild(commentsPara);
-
-//     //glue everything together
-//     commentsElem.appendChild(commentsContainer);
-//   }
-// }
-
-// displayComments();
-
-//Form functionality
-
-// const form = document.getElementById("main-form");
-
-// form.addEventListener("submit", function (event) {
-//   event.preventDefault();
-//   console.log("form submitted");
-//   console.log(event.target.name.value);
-//   console.log(event.target.message.value);
-
-//   const addNewComment = {
-//     name: event.target.name.value,
-//     comments: event.target.message.value,
-//     date: new Date(Date.now()).toLocaleDateString(),
-//     avatar: "./Assets/Images/Mohan-muruge.jpg",
-//   };
-
-//   newComments.unshift(addNewComment);
-
-//   form.reset();
-
-//   displayComments();
-// });
+//POST comments to API
 
 const form = document.getElementById("main-form");
-
-
 
 form.addEventListener("submit", handleAddComment);
 
@@ -214,10 +100,8 @@ function handleAddComment(event) {
       console.log(response.data);
       displayComments();
       event.target.reset();
-      
-    }) 
+    })
     .catch(() => {
-      console.log('error adding to api');
+      console.log("error adding to api");
     });
 }
-
